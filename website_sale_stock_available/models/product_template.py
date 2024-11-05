3c4e8e5b032cb763d9827d5aa6f4f5563f7de7ed
jbaldellou <jbaldellou@nodobox.com>
2024-11-05 08:25:12 -0300

    [MOD]Ticket 8736 05/11/2024.

--- website_sale_stock_available/models/product_template.py
+++ website_sale_stock_available/models/product_template.py
@@ -12,7 +12,6 @@ class ProductTemplate(models.Model):
         combination=False,
         product_id=False,
         add_qty=1,
-        pricelist=False,
         parent_combination=False,
         only_template=False,
     ):
@@ -21,7 +20,6 @@ class ProductTemplate(models.Model):
             combination,
             product_id,
             add_qty,
-            pricelist,
             parent_combination,
             only_template,
         )
