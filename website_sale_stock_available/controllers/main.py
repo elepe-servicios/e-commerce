e71fd1b7d1c865d4a86d2bb6cb93b7fddeadb79c
Leonardo Dibarbora <leonardodibarbora@elepeservicios.com.ar>
2025-01-31 16:58:23 -0300

    [MOD] Ticket #9272: Provider checkout fix

--- website_sale_stock_available/controllers/main.py
+++ website_sale_stock_available/controllers/main.py
@@ -6,10 +6,12 @@ from odoo.addons.website_sale_stock.controllers.main import website_sale_control
 
 
 class WebsiteSale(website_sale_controller.WebsiteSale):
-    @route()
-    def shop_payment_transaction(self, *args, **kwargs):
+    @route(
+        '/shop/payment/transaction/<int:order_id>', type='json', auth='public', website=True
+    )
+    def shop_payment_transaction(self, order_id, access_token, **kwargs):
         """Inject a context when potential or promised stock is set"""
         request.website = request.website.with_context(
             website_sale_stock_available=True
         )
-        return super().shop_payment_transaction(*args, **kwargs)
+        return super().shop_payment_transaction(order_id, access_token, **kwargs)
